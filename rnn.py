39d46485a7774b9bc8c478d255a2913f092aaebf
sameet <sameetsabu1@gmail.com>
2021-12-17 14:10:43 +0530

    predicted stock price

--- rnn.py
+++ rnn.py
@@ -1,9 +1,69 @@
-
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-dataset=pd.read_csv('Google_Stock_Price_Train.csv')
-training_set=dataset.iloc[:,1:2].values
+dataset_train=pd.read_csv('Google_Stock_Price_Train.csv')
+training_set=dataset_train.iloc[:,1:2].values
+
+from sklearn.preprocessing import MinMaxScaler
+sc=MinMaxScaler(feature_range=(0,1))
+training_set_scaled=sc.fit_transform(training_set)
 
 
+x_train=[]
+y_train=[]
+for i in range(60,1258):
+    x_train.append(training_set_scaled[i-60:i,0])
+    y_train.append(training_set_scaled[i,0])
+x_train,y_train=np.array(x_train),np.array(y_train)
+
+x_train=np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+regressor=Sequential()
+regressor.add(LSTM(units=50,return_sequences=True,input_shape=(x_train.shape[1],1)))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units=50,return_sequences=True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units=50,return_sequences=True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units=50))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units=1))
+
+regressor.compile(optimizer='adam',loss='mean_squared_error')
+
+regressor.fit(x_train,y_train,epochs=100,batch_size=32)
+
+dataset_test=pd.read_csv('Google_Stock_Price_Test.csv')
+test_set=dataset_test.iloc[:,1:2].values
+
+dataset_total= pd.concat((dataset_train['Open'],dataset_test['Open']),axis=0)
+inputs=dataset_total[len(dataset_total)-len(dataset_test)-60:].values
+inputs=inputs.reshape(-1,1)
+inputs=sc.transform(inputs)
+x_test=[]
+for i in range(60,80):
+    x_test.append(inputs[i-60:i,0])
+x_test=np.array(x_test)
+x_test=np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
+pred=regressor.predict(x_test)
+pred=sc.inverse_transform(pred)
+
+plt.plot(test_set,color='red',label='real stock price')
+plt.plot(pred,color='blue',label='predicted stock price')
+plt.title('google stock price')
+plt.xlabel('time')
+plt.label('price')
+plt.legend()
+plt.show()
+
+    
